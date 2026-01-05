675c0fb4aff0c4ac7d1b3500cbaba7666568b67f
Carolina Figueiredo <carolinasfigueiredo@hotmail.com>
2026-01-05 14:45:53 -0300

    Deploy do Projeto blog Pessoal

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -2,29 +2,36 @@ import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
 import { Tema } from "../../tema/entities/tema.entity";
 import { Usuario } from "../../usuario/entities/usuario.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({ name: "tb_postagens" })   // Indicando que a classe é uma Entidade/Model
 export class Postagem {
 
+    @ApiProperty()
     @PrimaryGeneratedColumn()   // Chave Primária e Auto Incremental
     id: number;
 
+    @ApiProperty()
     @IsNotEmpty()   // Validador de Objeto
     @Column({ length: 100, nullable: false })   // Tamanho Máximo: 100 | Regra do MySQL - NOT NULL
     titulo: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @Column({ length: 1000, nullable: false })   // Tamanho Máximo: 1000 | Regra do MySQL - NOT NULL
     texto: string;
 
+    @ApiProperty()
     @UpdateDateColumn()
     data: Date;
 
+    @ApiProperty({ type: () => Tema })  
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
         onDelete: "CASCADE"
     })
     tema: Tema
 
+    @ApiProperty({ type: () => Usuario })  
     @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
         onDelete: "CASCADE"
     })
